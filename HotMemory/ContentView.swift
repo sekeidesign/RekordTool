73595e1f9f7b7a621ac9dea0f0a5af68c91ab24c
Pg Gonni <59372630+sekeidesign@users.noreply.github.com>
2024-11-29 22:14:15 -0500

    Initial Commit

---
+++ HotMemory/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HotMemory
+//
+//  Created by Piergiorgio Gonni on 2024-11-29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HotMemory/HotMemoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotMemoryApp.swift
+//  HotMemory
+//
+//  Created by Piergiorgio Gonni on 2024-11-29.
+//
+
+import SwiftUI
+
+@main
+struct HotMemoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
